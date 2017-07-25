af872cc4e4a25dc8611de6251807c303e966479f
Mephymony <mebinmony@gmail.com>
2017-07-25 17:20:12 +0530

    Update ascii.java

---
+++ ascii.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+public static void main(String args[])
+{
+int i;
+for(int i=0;i<26;i++)
+{
+System.out.println((char)('A'+i) + " "+('A'+i));
+}
+}
+}
+
