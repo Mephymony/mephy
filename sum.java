639d23e71a7fb3d25d30914a4ad3a010bfd58aa5
Mephymony <mebinmony@gmail.com>
2017-07-25 17:54:59 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+public static void main(STring args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter number :");
+int n=sc.nextInt();
+int sum=0;
+for(int i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+System.out.println(sum);
+}
+}
